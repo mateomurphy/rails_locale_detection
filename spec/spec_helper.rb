68d81fd320b648ae1f957fdd90d3795cd7a133dd
Mateo Murphy <mateo.murphy@gmail.com>
2013-03-30 14:40:58 -0400

    remove extraneous requires in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,5 @@
 require 'rspec'
-require 'action_dispatch/middleware/cookies'
 require 'timecop'
-require 'rails'
-require 'active_support/core_ext'
-require 'i18n'
-require 'http_accept_language'
 require 'rails_locale_detection'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
