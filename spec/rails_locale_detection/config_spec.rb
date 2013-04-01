d5baec6adef4f8a6681f19c4c21b91c69a71c357
Mateo Murphy <mateo.murphy@gmail.com>
2013-04-01 18:21:04 -0400

    Add spec for backwards compatibility

--- spec/rails_locale_detection/config_spec.rb
+++ spec/rails_locale_detection/config_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe RailsLocaleDetection do
-
+  describe 'backwards compatility' do
+    it 'should be possible to configure using the previous module name' do
+      Rails::LocaleDetection.should respond_to(:config)
+    end
+  end
+  
   describe '.locale_expiry' do
     it "is set to 3 months by default" do
       RailsLocaleDetection.config do |c|
