9654b470c9e6675ef5e6ad27e16a0bb99f040350
Mateo Murphy <mateo.murphy@gmail.com>
2013-04-02 10:31:04 -0400

    Add an available locales accessor

--- lib/rails_locale_detection/locale_accessors.rb
+++ lib/rails_locale_detection/locale_accessors.rb
@@ -1,11 +1,11 @@
 module RailsLocaleDetection
   module LocaleAccessors
-    def available_locales
-      I18n.available_locales
+    def alternate_locales
+      available_locales - [current_locale]
     end
 
-    def default_locale
-      I18n.default_locale
+    def available_locales
+      I18n.available_locales
     end
 
     def current_locale
@@ -14,6 +14,10 @@ module RailsLocaleDetection
     
     def current_locale=(locale)
       I18n.locale = locale
+    end
+
+    def default_locale
+      I18n.default_locale
     end
   end
 end
