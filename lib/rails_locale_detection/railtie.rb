fca95cd92f7162df4ad413ec1a6f0128190483cf
Mateo Murphy <mateo.murphy@gmail.com>
2013-04-02 10:33:08 -0400

    Mix locale accessors into views as well

--- lib/rails_locale_detection/railtie.rb
+++ lib/rails_locale_detection/railtie.rb
@@ -4,6 +4,10 @@ module RailsLocaleDetection
       ActiveSupport.on_load(:action_controller) do
         ActionController::Base.send :include, RailsLocaleDetection::ControllerMethods
       end
+      
+      ActiveSupport.on_load(:action_view) do
+        ActionView::Base.send :include, Rails::LocaleDetection::LocaleAccessors
+      end      
     end
   end
 end
